7748227ee6082050e79565f7b725ebedcbba4307
Erik Youngren <artanis.00@gmail.com>
2011-11-17 12:35:59 -0800

    Remove unneeded nose requirement.
    Users don't NEED nose to run this, and developers should already have it or know how to get it.

--- setup.py
+++ setup.py
@@ -52,10 +52,6 @@ setup(
 
     packages=find_packages(exclude=['doc', 'ez_setup', 'examples', 'tests']),
 
-    install_requires=[
-    'nose >= 0.10.1',
-    ],
-
     zip_safe=False,
 
     package_data={
